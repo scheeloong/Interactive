d192391d14642538b8ad43896609b10ac44641d5
scheeloong <scheeloong@gmail.com>
2017-12-16 21:21:08 -0500

    .

--- examples/main.py
+++ examples/main.py
@@ -33,7 +33,8 @@ from matplotlib.widgets import Slider, Button, RadioButtons
 fig, ax = plt.subplots()
 plt.subplots_adjust(left=0.25, bottom=0.25)
 t = np.arange(0.0, 1.0, 0.001)
-a0 = 5 f0 = 3
+a0 = 5
+f0 = 3
 s = a0*np.sin(2*np.pi*f0*t)
 l, = plt.plot(t, s, lw=2, color='red')
 plt.axis([0, 1, -10, 10])
